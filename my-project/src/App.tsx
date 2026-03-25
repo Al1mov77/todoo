8259d6d5d8564828bfef5308829f2db6dfff1384
Al1mov77 <umarlegaaas444@gmail.com>
2026-03-25 13:05:48 +0500

    first commit

---
+++ my-project/src/App.tsx
@@ -0,0 +1,151 @@
+import React, { useEffect, useState } from 'react'
+import './App.css'
+import { useTodo } from './Todo'
+import { Box, Button, Modal, TextField } from '@mui/material';
+import { useForm } from 'react-hook-form';
+ let apiImg = "http://37.27.29.18:8001/images";
+
+function App() {
+  const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+  useEffect(() =>{
+getData()
+  }, [])
+     
+  const {data, getData, deleteData, addData, editData, deleteImage}:any = useTodo()
+  const [idx,setidx] = useState(null)
+  const {register,handleSubmit,watch, setValue,reset,formState:{errors}} = useForm()
+  const onsubmit = (data:any) =>{
+const formData = new FormData()
+   if(idx!=null){
+    editData({
+      id:idx,
+      name:data.name,
+      description:data.description
+    })
+   }
+   else{
+    const formData = new FormData()
+    formData.append("Name", data.name)
+    formData.append("Description", data.description)
+    if(data.images && data.images.length > 0){
+      for(let i=0;i<data.images.length;i++){
+              formData.append("Images", data.images[i])
+      }
+
+    }
+       addData(formData)
+   }
+   handleClose()
+   handleCloseEdit()
+   reset()
+   setidx(null)
+    
+    }
+    function handleEdit(user){
+      setidx(user.id)
+  setValue("name", user.name)
+  setValue("description", user.description)
+      handleOpenEdit()
+      
+    }
+ 
+  
+    const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const [openEdit, setOpenEdit] = React.useState(false);
+  const handleOpenEdit = () => setOpenEdit(true);
+  const handleCloseEdit = () => setOpenEdit(false);
+  return (
+    <>
+    <div className='flex justify-center items-center p-15'>
+<Button onClick={handleOpen} variant='outlined'>Add New</Button>
+    </div>
+    <div className='flex justify-between p-15 flex-wrap'>
+    
+      {data?.map((user:any) =>{
+        return <div className='relative rounded-2xl text-center shadow-2xl p-10 w-100 hover:translate-y-[-10px] transition-all duration-300 ease-in-out'>
+          <p className='font-bold text-2xl'>{user.name}</p>
+          <p className='font-semibold'>{user.age}</p>
+          <p className='font-semibold'>{user.description}</p>
+          <div className="flex justify-center mb-3">
+          {user.images?.map((img) => (
+            <div key={img.id}>
+              <img
+                className="h-[180px] w-[180px] rounded-[50%]"
+                src={`${apiImg}/${img.imageName}`}
+                alt=""
+              />
+              <br />
+              <div className='flex items-center justify-center gap-5'>
+       <Button onClick={() => deleteImage(img.id)}
+               
+variant='outlined'
+ color='error'
+>
+                DelImg
+              </Button>
+              <Button variant='outlined'>Add Image</Button>
+              </div>
+       
+            </div>
+          ))}<div/>
+      </div>
+      <div className='flex gap-17 items-center justify-center'>
+              <Button onClick={() => deleteData(user.id)} color='error' variant='outlined'>Delete</Button>
+              <Button onClick={() => handleEdit(user)} color='warning' variant='outlined'>Edit</Button>
+      </div>
+
+        </div>
+      })}
+      </div>
+      <Modal
+  open={open}
+  onClose={handleClose}
+  aria-labelledby="modal-modal-title"
+  aria-describedby="modal-modal-description"
+>
+  <Box sx={style}>
+    <form onSubmit={handleSubmit(onsubmit)} action="">
+   <TextField {...register("name")} fullWidth label="User Name" /> <br /> <br />
+   <input multiple {...register("images")}  type="file" />
+   <TextField {...register("description")} fullWidth label="User Description" /> <br /> <br /> <br />
+   <Button type='submit' fullWidth color='success' variant='contained'>Save</Button>
+    </form>
+
+  </Box>
+</Modal>
+
+
+  <Modal
+  open={openEdit}
+  onClose={handleCloseEdit}
+  aria-labelledby="modal-modal-title"
+  aria-describedby="modal-modal-description"
+>
+  <Box sx={style}>
+    <form onSubmit={handleSubmit(onsubmit)} action="">
+   <TextField {...register("name")} fullWidth label="User Name" /> <br /> <br />
+   <input multiple {...register("images")}  type="file" />
+   <TextField {...register("description")} fullWidth label="User Description" /> <br /> <br /> <br />
+   <Button type='submit' fullWidth color='success' variant='contained'>Save</Button>
+    </form>
+
+  </Box>
+</Modal>
+    </>
+  )
+}
+
+export default App
---
+++ my-project/src/Todo.tsx
@@ -0,0 +1,48 @@
+import axios from "axios";
+import { create } from "zustand";
+let api = "http://37.27.29.18:8001/api/to-dos"
+
+export const useTodo = create((set, get) =>({
+    data:[],
+getData:( async() =>{
+try {
+    const {data} = await axios.get(api)
+    set(data)
+} catch (error) {
+    console.error(error);
+}
+}),
+deleteData:(async (id:number) =>{
+    try {
+        await axios.delete(`${api}?id=${id}`)
+        get().getData()
+    } catch (error) {
+        console.error(error);
+    }
+}),
+addData:(async (formData) =>{
+    try {
+        await axios.post(api,formData)
+        get().getData()
+    } catch (error) {
+        console.error(error);
+    }
+}),
+editData:(async (formData, id) =>{
+    try {
+        await axios.put(`${api}?id=${id}`, formData)
+        get().getData()
+    } catch (error) {
+        console.error(error);
+    }
+}),
+deleteImage:(async (id:number) =>{
+    try {
+        await axios.delete(`http://37.27.29.18:8001/api/to-dos/images/${id}`)
+        get().getData()
+    } catch (error) {
+        console.error(error);
+    }
+})
+
+}))
